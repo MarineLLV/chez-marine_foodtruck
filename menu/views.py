af9784b05a17e0bb189306c67fae7e62494264f7
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-22 11:29:22 +0100

    read objects from database

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Food
 
 
 # /menu
 def index(request):
-    return HttpResponse("Les pizzas")
+    foods = Food.objects.all()
+    foods_names = [food.name for food in foods]
+    foods_names_str = ", ".join(foods_names)
+    return HttpResponse("Les galettes : " + foods_names_str)
