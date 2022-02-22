54735e24dddf8705203c8be3ceec295f56c0f502
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-22 10:56:02 +0100

    improve admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Food
 
 
-admin.site.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ingredients', 'vegetarian', 'price')
+    search_fields = ['name']
+
+admin.site.register(Food, FoodAdmin)
--- menu/models.py
+++ menu/models.py
@@ -6,3 +6,6 @@ class Food(models.Model):
     ingredients = models.CharField(max_length=400)
     price = models.FloatField(default=0)
     vegetarian = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
