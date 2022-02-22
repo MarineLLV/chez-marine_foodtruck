2ff90e5ae7217353d996391bef1ab39354c10d81
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-22 12:04:12 +0100

    pass data to templates

--- menu/views.py
+++ menu/views.py
@@ -5,8 +5,8 @@ from .models import Food
 
 # /menu
 def index(request):
-    # foods = Food.objects.all()
+    foods = Food.objects.all()
     # foods_names_and_prices = [food.name + " : " + str(food.price) + "€" for food in foods]
     # foods_names_and_prices_str = ", ".join(foods_names_and_prices)
     # return HttpResponse("Les galettes : " + foods_names_and_prices_str)
-    return render(request, 'menu/index.html')
+    return render(request, 'menu/index.html', {'foods': foods})
